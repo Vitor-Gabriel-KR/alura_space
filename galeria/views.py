e248236125b2a1a0bea3e0bf7d0444fda4f3a369
kroshaResheniye <116317977+Vitor-Gabriel-KR@users.noreply.github.com>
2024-05-04 04:33:22 -0300

    Final curso

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'galeria/index.html')
 
 def imagem(request):
-    return render(request,'imagem.html')
+    return render(request, 'galeria/imagem.html')
